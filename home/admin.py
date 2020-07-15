d37c43b78401a1c128ac00ffa383906255fd40e8
Vlad <glitzybunny@protonmail.ch>
2020-07-15 04:28:13 +0300

    changed design

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,6 @@
-
+from django.conf.urls import url
 from wagtail.contrib.modeladmin.options import ModelAdmin
-from home.models import Node
+from home.models import Node, NodeButtonHelper
 
 class NodeAdmin(ModelAdmin):
     """Class for presenting topics in admin using modeladmin."""
@@ -8,16 +8,34 @@ class NodeAdmin(ModelAdmin):
     model = Node
 
     # admin menu options
-    menu_icon = 'fa-cube'  # using wagtail-fontawesome
+    menu_icon = 'fa-cube'  # using wagtail fontawesome
     menu_order = 800
 
     # listing view options
+    list_display = ('get_as_listing_header', 'get_parent', 'aliases')
     list_per_page = 50
-    # list_display = ('name', 'get_parent', 'aliases')
     search_fields = ('name', 'aliases')
 
     # inspect view options
     inspect_view_enabled = True
     inspect_view_fields = ('name', 'get_parent', 'aliases', 'id')
 
-    list_display = ('get_as_listing_header', 'get_parent', 'aliases')
+    # other overrides
+    button_helper_class = NodeButtonHelper
+
+    def add_child_view(self, request, instance_pk):
+        """Generate a class-based view to provide 'add child' functionality."""
+        # instance_pk will become the default selected parent_pk
+        kwargs = {'model_admin': self, 'parent_pk': instance_pk}
+        view_class = AddChildNodeViewClass
+        return view_class.as_view(**kwargs)(request)
+
+    def get_admin_urls_for_registration(self):
+        """Add the new url for add child page to the registered URLs."""
+        urls = super().get_admin_urls_for_registration()
+        add_child_url = url(
+            self.url_helper.get_action_url_pattern('add_child'),
+            self.add_child_view,
+            name=self.url_helper.get_action_url_name('add_child')
+        )
+        return urls + (add_child_url, )
--- home/models.py
+++ home/models.py
@@ -23,23 +23,6 @@ from blog.models import BlogPage
 
 
 
-class Category(MP_Node):
-    name = models.CharField(max_length=30)
-
-    node_order_by = ['name']
-
-    panels = [
-        FieldPanel('parent'),  # virtual field - see TopicForm later
-        FieldPanel('name'),
-        FieldPanel('aliases', widget=forms.Textarea(attrs={'rows': '5'})),
-    ]
-
-
-    def __str__(self):
-        return 'Category: %s' % self.name
-
-
-
 node_name_validator = RegexValidator(
     regex='^[\w][a-zA-Z &]+$',
     message="Letters, numbers and '&' only plus must start with a letter.",
@@ -252,52 +235,13 @@ class AddChildNodeViewClass(CreateView):
         return {'parent': self.parent_pk}
 
 
-class NodeAdmin(ModelAdmin):
-    """Class for presenting topics in admin using modeladmin."""
 
-    model = Node
-
-    # admin menu options
-    menu_icon = 'fa-cube'  # using wagtail fontawesome
-    menu_order = 800
-
-    # listing view options
-    list_display = ('get_as_listing_header', 'get_parent', 'aliases')
-    list_per_page = 50
-    search_fields = ('name', 'aliases')
-
-    # inspect view options
-    inspect_view_enabled = True
-    inspect_view_fields = ('name', 'get_parent', 'aliases', 'id')
-
-    # other overrides
-    button_helper_class = NodeButtonHelper
-
-    def add_child_view(self, request, instance_pk):
-        """Generate a class-based view to provide 'add child' functionality."""
-        # instance_pk will become the default selected parent_pk
-        kwargs = {'model_admin': self, 'parent_pk': instance_pk}
-        view_class = AddChildNodeViewClass
-        return view_class.as_view(**kwargs)(request)
-
-    def get_admin_urls_for_registration(self):
-        """Add the new url for add child page to the registered URLs."""
-        urls = super().get_admin_urls_for_registration()
-        add_child_url = url(
-            self.url_helper.get_action_url_pattern('add_child'),
-            self.add_child_view,
-            name=self.url_helper.get_action_url_name('add_child')
-        )
-        return urls + (add_child_url, )
 
 class HomePage(Page):
     template = 'home/home_page.html'
 
     # title = models.CharField(max_length=120, blank=True, null=True)
-    
-    
-    # banner_title = models.CharField(max_length=100, blank=True, null=True)
-    # intro = RichTextField(blank=True)
+    # intro = RichTextField(blank=True, null=True)
  
     def get_context(self, request):
         # Обновляем контекст для внесения только опубликованных постов в обратном хронологическом порядке
@@ -311,6 +255,7 @@ class HomePage(Page):
     
     content_panels = Page.content_panels + [
         InlinePanel('slider_items', label="Slider"),
+        FieldPanel('title')
     ]
 
 # class LinkFields(models.Model):
