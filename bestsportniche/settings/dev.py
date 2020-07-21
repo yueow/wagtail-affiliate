dda38ac006ee40d4c82bb8b6b3f8f66bff03f6c5
Vlad <glitzybunny@protonmail.ch>
2020-07-21 21:48:24 +0300

    Got ready for deployment

--- bestsportniche/settings/dev.py
+++ bestsportniche/settings/dev.py
@@ -1,13 +1,18 @@
+import os
 from .base import *
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == "True")
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q9x0npzk@#e4hfy)0h$ix*=bl$lh2pq&zf_t0e0a#+z#^=d=t('
+# SECRET_KEY = 'q9x0npzk@#e4hfy)0h$ix*=bl$lh2pq&zf_t0e0a#+z#^=d=t('
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ['*'] 
+# ALLOWED_HOSTS = ['*'] 
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'herokuapp', 'aws-service'] 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
